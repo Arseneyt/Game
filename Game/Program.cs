b137975e762d0a259be3fd433ebb569d04eeb923
Arseneyt <arseneyt@yandex.ru>
2023-04-08 12:21:14 +0500

    first

---
+++ Game/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+ 
+namespace Game1
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+ 
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+ 
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+ 
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            texture = Content.Load<Texture2D>("SpriteBatch");
+        }
+ 
+        protected override void UnloadContent()
+        {
+ 
+        }
+ 
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+ 
+            base.Update(gameTime);
+        }
+         
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // отрисовка спрайта
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+            spriteBatch.End();
+             
+            base.Draw(gameTime);
+        }
+    }
+
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using var game = new Game1();
+            game.Run();
+        }
+    }
+}
